af925d7addbdd808c163bd8846602c2753e87753
Jorge Corzo <jorgeach111@hotmail.com>
2024-01-06 23:24:24 +1030

    base layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      background: "#1E2020",
+      text: "#FFFFFF",
+      primary: "#ECA53A",
+      secondary: "#E8CC8D",
+      accent: "#048389",
+      error: "#5D2E4D",
+      success: "#54AA9F",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+};
